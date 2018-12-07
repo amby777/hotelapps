8a97a6a500ed3fc3b6a5ec9dc287a2d4a8aa9936
Ambrose <ambyuk22@gmail.com>
2018-12-07 19:19:38 +0000

    customised typography font sizes,improved navtabs, worked on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AccomodationComponent } from './accomodation/accomodation.component';
 import { CelebrationsComponent } from './celebrations/celebrations.component';
 import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
@@ -14,6 +15,9 @@ const routes: Routes = [
     { path: 'accomodation', component: AccomodationComponent },
     { path: 'celebrations', component: CelebrationsComponent },
     { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+    { path: 'foodandbeverage',
+      redirectTo: 'food-and-beverage',
+      pathMatch: 'full' },
     { path: 'lifestyle', component: LifestyleComponent },
     { path: 'weddings', component: WeddingsComponent },
     { path: 'offers', component: OffersComponent },
